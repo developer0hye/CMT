bc9a0f1d6c6b9e2fe91772048d64025d3a8e5085
Yonghye Kwon <developer.0hye@gmail.com>
2021-08-31 21:50:09 +0900

    building...

--- cmt.py
+++ cmt.py
@@ -1,1 +1,13 @@
+import torch
+import torch.nn as nn
 
+class CMT(nn.Module):
+    def __init__(self,
+                 stem_channels: list):
+        super(CMT, self).__init__()
+        
+        self.stem
+    
+    def forward(self, x):
+        pass
+        
